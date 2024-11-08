5bbfe72cc8a86564850d0fe74c73ab55601f27bb
Niko Alteri <nikoalteri@Mac.station>
2024-11-08 14:22:19 +0100

    updated API key

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { Box, Stack, Typography, Button } from "@mui/material";
+import React from 'react';
+import { Box, Typography, Button } from '@mui/material';
 
-import HeroBannerImage from "../assets/images/banner.png";
+import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => {
   return (
     <Box
       sx={{
-        mt: { lg: "212px", xs: "70px" },
-        ml: { sm: "50px" },
+        mt: { lg: '212px', xs: '70px' },
+        ml: { sm: '50px' },
       }}
       position="relative"
       p="20px"
@@ -18,15 +18,21 @@ const HeroBanner = () => {
       </Typography>
       <Typography
         fontWeight={700}
-        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
-        mb="23px" mt="30px"
+        sx={{ fontSize: { lg: '44px', xs: '40px' } }}
+        mb="23px"
+        mt="30px"
       >
         Sweat, Smile <br /> and Repeat
       </Typography>
       <Typography fontSize="22px" lineHeight="35px" mb={4}>
         Check out the most effective exercises
       </Typography>
-      <Button variant="contained" color="error" href="#exercises" sx={{ backgroundColor: "#FF2625", padding: "15px"}}>
+      <Button
+        variant="contained"
+        color="error"
+        href="#exercises"
+        sx={{ backgroundColor: '#FF2625', padding: '15px' }}
+      >
         Explore Exercises
       </Button>
       <Typography
@@ -34,7 +40,7 @@ const HeroBanner = () => {
         color="#FF2625"
         sx={{
           opacity: 0.1,
-          display: { lg: "block", xs: "none" },
+          display: { lg: 'block', xs: 'none' },
         }}
         fontSize="200px"
       >
